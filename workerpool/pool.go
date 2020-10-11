b4a27aeeeaa77cfcd36ff1b8bd5df3f9bb3e9ad1
TEJA RAVI C KALAGARA <tejaravickalagara@TEJAs-MacBook-Pro.local>
2020-10-11 14:55:38 -0700

    updated with jon timout

--- workerpool/pool.go
+++ workerpool/pool.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 
+	jobTimeOut := 2 * time.Second
 	workers := 3
 	jobs := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	var jobResults []int
@@ -16,7 +17,7 @@ func main() {
 	outputChan := make(chan int, len(jobs))
 
 	// initiating woker pool
-	initaiatePool(workers, inputChan, outputChan)
+	initaiatePool(workers, jobTimeOut, inputChan, outputChan)
 
 	// feeding the pool
 	for _, job := range jobs {
@@ -37,21 +38,33 @@ func main() {
 
 }
 
-func initaiatePool(workers int, requestChannel chan int, reponseChannel chan int) {
+func initaiatePool(workers int, jobTimeOut time.Duration, requestChannel, reponseChannel chan int) {
 	for i := 1; i <= workers; i++ {
-		go worker(i, requestChannel, reponseChannel)
+		go worker(i, jobTimeOut, requestChannel, reponseChannel)
 	}
 }
 
-func worker(workerID int, requestChannel chan int, reponseChannel chan int) {
+func worker(workerID int, jobTimeOut time.Duration, requestChannel, reponseChannel chan int) {
 	for job := range requestChannel {
 		log.Printf("worker %v processing job %v", workerID, job)
-		reponseChannel <- run(job)
+		reponseChannel <- run(job, jobTimeOut)
 	}
 }
 
-func run(job int) int {
-	// job impl
+func run(job int, jobTimeOut time.Duration) int {
+	rChan := make(chan int, 1)
+	go callDownstream(job, rChan)
+	select {
+	case r := <-rChan:
+		return r
+	case <-time.After(jobTimeOut):
+		close(rChan)
+		log.Printf("Downstream timeout for job: %v", job)
+		return 0
+	}
+}
+
+func callDownstream(job int, rChan chan int) {
 	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-	return job
+	rChan <- job
 }
